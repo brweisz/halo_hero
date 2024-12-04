e9ab7e823d2f8c69d07975674a11676415847a9a
bruno <bweisz@eryxsoluciones.com.ar>
2024-12-04 16:06:31 -0300

    almost there

--- bit_operations/src/main.rs
+++ bit_operations/src/main.rs
@@ -1,5 +1,5 @@
 use std::convert::TryInto;
-use std::iter::{IntoIterator, Iterator};
+use std::iter::{Iterator};
 use std::marker::PhantomData;
 
 use halo2_proofs::{
@@ -9,7 +9,7 @@ use halo2_proofs::{
 };
 
 use ff::{Field, PrimeField};
-use halo2_proofs::circuit::{Table, Value};
+use halo2_proofs::circuit::{Value};
 use halo2_proofs::plonk::{Advice, Column, Expression, Selector, TableColumn};
 use halo2_proofs::poly::Rotation;
 
@@ -35,8 +35,6 @@ const TABLE_OF_BIT_OPERATIONS: [[u8; 4]; 12] = [
     [2,1,1,1],
 ];
 
-const RANGE_TABLE_U8: [u8; 256] = (0..256).into_iter().try_into().unwrap();
-
 #[derive(Clone, Debug)]
 struct U8Chip<F: Field + Clone> {
     _ph: PhantomData<F>,
@@ -53,26 +51,27 @@ struct U8Chip<F: Field + Clone> {
 }
 
 #[derive(Clone, Debug)]
-struct TestConfig<F: Field + PrimeField + Clone> {
+struct TestConfig<F: Field + Clone> {
     _ph: PhantomData<F>,
     advice: Column<Advice>,
     u8_chip: U8Chip<F>,
 }
 
-impl<F: Field + PrimeField> U8Chip<F> {
+impl<F: PrimeField> U8Chip<F> {
     fn new_for(meta: &mut ConstraintSystem<F>, advice: Column<Advice>) -> Self {
         let bits = [meta.advice_column(); 8];
         let t_range = meta.lookup_table_column();
         let q_decomposed = meta.selector();
 
-        meta.lookup(||"Range check u8", |meta|{
+        meta.lookup("Range check u8", |meta|{
            let advice_value = meta.query_advice(advice, Rotation::cur());
-           vec![(q_decomposed * advice_value, t_range)]
+            let q_decomposed_ = meta.query_selector(q_decomposed);
+           vec![(q_decomposed_ * advice_value, t_range)]
         });
 
-        meta.create_gate(||"u8 decomposed", |meta|{
-            let bits_: [Expression<F>; 8] = bits.iter().map(|column|{
-                meta.query_advice(*column, Rotation::cur())
+        meta.create_gate("u8 decomposed", |meta|{
+            let bits_: Vec<Expression<F>> = bits.into_iter().map(|column|{
+                meta.query_advice(column, Rotation::cur())
             }).collect();
             let advice_value = meta.query_advice(advice, Rotation::cur());
             let q_decomposed = meta.query_selector(q_decomposed);
@@ -87,17 +86,18 @@ impl<F: Field + PrimeField> U8Chip<F> {
                 q_decomposed.clone() * (bits_[7].clone() * (bits_[7].clone() - Expression::Constant(F::ONE))),
                 q_decomposed.clone() * (advice_value -
                     bits_[0].clone() -
-                    bits_[1].clone() * Expression::Constant(F::from_u128(1<<1)) -
-                    bits_[2].clone() * Expression::Constant(F::from_u128(1<<2)) -
-                    bits_[3].clone() * Expression::Constant(F::from_u128(1<<3)) -
-                    bits_[4].clone() * Expression::Constant(F::from_u128(1<<4)) -
-                    bits_[5].clone() * Expression::Constant(F::from_u128(1<<5)) -
-                    bits_[6].clone() * Expression::Constant(F::from_u128(1<<6)) -
-                    bits_[7].clone() * Expression::Constant(F::from_u128(1<<7))
+                    bits_[1].clone() * Expression::Constant(F::from(1<<1)) -
+                    bits_[2].clone() * Expression::Constant(F::from(1<<2)) -
+                    bits_[3].clone() * Expression::Constant(F::from(1<<3)) -
+                    bits_[4].clone() * Expression::Constant(F::from(1<<4)) -
+                    bits_[5].clone() * Expression::Constant(F::from(1<<5)) -
+                    bits_[6].clone() * Expression::Constant(F::from(1<<6)) -
+                    bits_[7].clone() * Expression::Constant(F::from(1<<7))
                 )
             ]
         });
         Self {
+            _ph: PhantomData,
             bits,
             t_range,
             q_decomposed,
@@ -105,18 +105,18 @@ impl<F: Field + PrimeField> U8Chip<F> {
     }
 }
 
-impl<F: Field> TestCircuit<F>{
-    fn set_lookup_table_u8(&self, mut layouter: impl Layouter<F>, config: <TestCircuit<F> as Circuit<F>>::Config){
-        layouter.assign_table(|| "Range Check u8", |mut table| {
-            for i in 0..256 {
-                table.assign_cell(|| "Range check u8 table", config.u8_chip.t_range, i, Value::known(i))?;
+impl<F: Field + PrimeField> TestCircuit<F>{
+    fn set_lookup_table_u8(&self, mut layouter: impl Layouter<F>, config: &TestConfig<F>){
+        let _ = layouter.assign_table(|| "Range Check u8", |mut table| {
+            for i in 0..256u128 {
+                table.assign_cell(|| "Range check u8 table", config.u8_chip.t_range, i as usize, ||Value::known(F::from_u128(i)))?;
             }
             Ok(())
-        })?;
+        });
     }
 }
 
-impl<F: Field> Circuit<F> for TestCircuit<F> {
+impl<F: Field + PrimeField> Circuit<F> for TestCircuit<F> {
     type Config = TestConfig<F>;
     type FloorPlanner = SimpleFloorPlanner;
 
@@ -141,8 +141,19 @@ impl<F: Field> Circuit<F> for TestCircuit<F> {
         config: Self::Config,
         mut layouter: impl Layouter<F>,
     ) -> Result<(), plonk::Error> {
-        self.set_lookup_table_u8(&mut layouter, &config);
+        self.set_lookup_table_u8(&layouter, &config);
 
+        let _ = layouter.assign_region(||"Pruebita", |mut region| {
+            config.u8_chip.q_decomposed.enable(&mut region, 0)?;
+            region.assign_advice(||"Valor de prueba", config.advice, 0, || Value::known(F::from(7)))?;
+            region.assign_advice(||"Descomposicion en bits", config.u8_chip.bits[0], 0, || Value::known(F::from(1)))?;
+            region.assign_advice(||"Descomposicion en bits", config.u8_chip.bits[1], 0, || Value::known(F::from(1)))?;
+            region.assign_advice(||"Descomposicion en bits", config.u8_chip.bits[2], 0, || Value::known(F::from(1)))?;
+            for i in 3..8 {
+                region.assign_advice(||"Descomposicion en bits", config.u8_chip.bits[i], 0, || Value::known(F::from(0)))?;
+            }
+            Ok(())
+        });
 
 
         Ok(())
